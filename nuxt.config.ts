71a8f0d1b2eacc35d0a4f2a2b182de7322241129
AdityaAWP <aditwpratama1@gmail.com>
2025-04-02 01:03:59 +0700

    Add Tailwind and Archivo font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite"
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  modules: ['@nuxt/content'],
-  devtools: { enabled: true }
+  modules: ['@nuxt/content', '@nuxt/fonts'],
+  devtools: { enabled: true },
+  fonts:{
+    families: [
+      {
+        name:'Archivo', provider:'google'
+      }
+    ]
+  },
+  css: ['~/assets/css/main.css'],
+  vite:{
+    plugins:[tailwindcss()]
+  }
 })
--- pages/documentation/[slug].vue
+++ pages/documentation/[slug].vue
@@ -1,4 +1,15 @@
 <template>
-    <h1>Ini Buat Docs</h1>
+    <div class="px-10 flex justify-between font-archivo">
+        <h1 class="text-[48px]"><span class="text-[#007163]">TeaLinux</span> Docs</h1>  
+        <div>
+            <h1>Search</h1>
+        </div>
+    </div>
+    <div class="grid grid-cols-12 min-h-screen">
+        <div class="col-span-2 bg-amber-300"><h1>hellow</h1></div>
+        <div class="col-span-8 bg-blue-300"></div>
+        <div class="col-span-2 bg-amber-300"></div>
+    </div>
 </template>
 <script scoped></script>
+
