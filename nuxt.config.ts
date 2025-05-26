6b16e4e9616560c1f2864226a01ba70321d6c215
PRATANGGA <pratanggajulang31@gmail.com>
2025-05-26 20:59:50 +0700

    test nuxtjs/mdc

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import tailwindcss from "@tailwindcss/vite"
+import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  modules: ['@nuxt/content', '@nuxt/fonts'],
+  compatibilityDate: "2024-11-01",
+  modules: ["@nuxt/content", "@nuxt/fonts", "@nuxtjs/mdc"],
   devtools: { enabled: true },
-  fonts:{
+  fonts: {
     families: [
       {
-        name:'Archivo', provider:'google'
-      }
-    ]
+        name: "Archivo",
+        provider: "google",
+      },
+    ],
   },
-  css: ['~/assets/css/main.css'],
-  vite:{
-    plugins:[tailwindcss()]
-  }
-})
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
--- server/api/[slug].ts
+++ server/api/[slug].ts
@@ -1,7 +1,8 @@
 export default eventHandler(async (event) => {
-    const { slug } = getRouterParams(event)
-    const page = await queryCollection(event, 'documentation').path(`/documentation/${slug}`).first()
-    
-    return page
-  })
-  
+  const { slug } = getRouterParams(event);
+  const page = await queryCollection(event, "documentation")
+    .path(`/documentation/${slug}`)
+    .first();
+
+  return page;
+});
