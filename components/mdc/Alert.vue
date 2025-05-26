4f65e193cb1a0a1c68bd9acc054020b996f07b2f
PRATANGGA <pratanggajulang31@gmail.com>
2025-05-27 00:33:05 +0700

    add dependency nuxtjs/mdc

--- components/mdc/Alert.vue
+++ components/mdc/Alert.vue
@@ -1,10 +1,14 @@
+<script lang="ts" setup>
+defineProps({});
+</script>
+
 <template>
   <div
-    class="flex items-center p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400"
+    class="relative flex items-center p-4 mb-4 text-green-400 bg-gray-900 border-l-4 border-green-500 rounded shadow-sm"
     role="alert"
   >
     <svg
-      class="shrink-0 inline w-4 h-4 me-3"
+      class="shrink-0 inline w-4 h-4 me-3 text-green-400"
       aria-hidden="true"
       xmlns="http://www.w3.org/2000/svg"
       fill="currentColor"
@@ -14,10 +18,8 @@
         d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"
       />
     </svg>
-    <span class="sr-only">Info</span>
-    <div>
-      <span class="font-medium"><slot></slot></span> Change a few things up and
-      try submitting again.
+    <div class="flex-grow">
+      <slot />
     </div>
   </div>
 </template>
---
+++ components/mdc/Heading.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1
+    class="text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"
+  >
+    <span
+      class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400"
+    >
+      <MDCSlot unwrap="p" />
+    </span>
+  </h1>
+</template>
