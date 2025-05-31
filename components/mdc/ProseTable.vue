e4ec75d2a66dcee0fece0a6eae7c3903dc306aa4
PRATANGGA <pratanggajulang31@gmail.com>
2025-05-31 09:53:44 +0700

    add component reusable in ProseTable

--- components/mdc/ProseTable.vue
+++ components/mdc/ProseTable.vue
@@ -1,3 +1,16 @@
+<script lang="ts" setup>
+defineProps({
+  headers: {
+    type: Array as () => string[],
+    required: true,
+  },
+  rows: {
+    type: Array as () => string[][],
+    required: true,
+  },
+});
+</script>
+
 <template>
   <div class="relative overflow-x-auto mt-5 mb-5">
     <table
@@ -7,47 +20,35 @@
         class="text-xs text-white-700 uppercase bg-blue-50 dark:bg-[#007163] dark:text-white-400"
       >
         <tr>
-          <th scope="col" class="px-6 py-3 text-white">Component</th>
-          <th scope="col" class="px-6 py-3 text-white">Requirement</th>
-          <th scope="col" class="px-6 py-3 text-white">Recommended</th>
+          <th
+            v-for="(header, index) in headers"
+            :key="index"
+            scope="col"
+            class="px-6 py-3 text-white"
+          >
+            {{ header }}
+          </th>
         </tr>
       </thead>
       <tbody>
         <tr
+          v-for="(row, rowIndex) in rows"
+          :key="rowIndex"
           class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200"
         >
-          <th
+          <td
+            v-for="(cell, cellIndex) in row"
+            :key="cellIndex"
+            :class="[
+              'px-6 py-4 text-white',
+              cellIndex === 0
+                ? 'font-medium text-gray-900 whitespace-nowrap dark:text-white'
+                : '',
+            ]"
             scope="row"
-            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
           >
-            CPU
-          </th>
-          <td class="px-6 py-4 text-white">
-            64-bit x86, ARM (RAS PI 4) architectures
+            {{ cell }}
           </td>
-          <td class="px-6 py-4 text-white"></td>
-        </tr>
-        <tr
-          class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200"
-        >
-          <th
-            scope="row"
-            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-          >
-            RAM
-          </th>
-          <td class="px-6 py-4 text-white">4 GB</td>
-          <td class="px-6 py-4 text-white">8 GB</td>
-        </tr>
-        <tr class="bg-white dark:bg-gray-800">
-          <th
-            scope="row"
-            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-          >
-            Laptop PC Drive Storage
-          </th>
-          <td class="px-6 py-4 text-white">20 GB</td>
-          <td class="px-6 py-4"></td>
         </tr>
       </tbody>
     </table>
