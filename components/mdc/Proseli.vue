b6a3fc3966f212a017928abc6c9a31d170db2ac8
PRATANGGA <pratanggajulang31@gmail.com>
2025-06-09 03:14:55 +0700

    crot

---
+++ components/mdc/Proseli.vue
@@ -0,0 +1,22 @@
+<template>
+  <li
+    class="text-[16px] text-justify font-light text-[#4A4A4A] dark:text-black-400 font-archivo"
+  >
+    <MDCSlot />
+  </li>
+  <li
+    class="text-[16px] text-justify font-light text-[#4A4A4A] dark:text-black-400 font-archivo"
+  >
+    <MDCSlot />
+  </li>
+  <li
+    class="text-[16px] text-justify font-light text-[#4A4A4A] dark:text-black-400 font-archivo"
+  >
+    <MDCSlot />
+  </li>
+  <li
+    class="text-[16px] text-justify font-light text-[#4A4A4A] dark:text-black-400 font-archivo"
+  >
+    <MDCSlot />
+  </li>
+</template>
