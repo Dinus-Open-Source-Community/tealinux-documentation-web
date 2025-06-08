04176f2bb588c7152350d293dee15eebd71cede2
AdityaAWP <aditwpratama1@gmail.com>
2025-06-08 23:41:09 +0700

    enhance Card and NavLink components with icon slots and update navigation links

--- components/mdc/Card.vue
+++ components/mdc/Card.vue
@@ -11,6 +11,9 @@ defineProps<{
     :to="href"
     class="block max-w-sm p-6 bg-[#D9D9D9] border border-gray-200 rounded-lg shadow hover:border-[#007163] hover:bg-[#007163]transition-colors duration-200 no-underline"
   >
+  <div class="my-3">
+    <slot name="icon"></slot>
+  </div>
     <h5
       class="mb-2 text-2xl font-bold tracking-tight text-[#007163] font-archivo"
     >
--- components/mdc/NavLink.vue
+++ components/mdc/NavLink.vue
@@ -16,23 +16,43 @@ const hasNext = computed(() => props.nextTitle && props.nextHref);
 </script>
 
 <template>
-  <div class="w-full max-w-6xl mx-auto mt-16 flex justify-between">
-    <div v-if="hasPrev" class="flex-1">
+  <div class="w-full max-w-6xl mx-auto mt-16 flex flex-col md:flex-row  justify-between">
+    <div  class="flex-1">
+      <div v-if="hasPrev">
       <Card
         :title="props.prevTitle!"
         :description="props.prevDescription!"
         :href="props.prevHref!"
-      />
+      >
+      <template #icon >
+          <div class="flex justify-start">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-10 text-primary">
+            <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-4.28 9.22a.75.75 0 0 0 0 1.06l3 3a.75.75 0 1 0 1.06-1.06l-1.72-1.72h5.69a.75.75 0 0 0 0-1.5h-5.69l1.72-1.72a.75.75 0 0 0-1.06-1.06l-3 3Z" clip-rule="evenodd" />
+          </svg>
+          </div>
+      </template>
+    </Card>
     </div>
+  </div>
 
-    <div v-if="!hasPrev && hasNext" class="flex-1"></div>
+    <div class="flex-1"></div>
 
-    <div v-if="hasNext" class="flex-1 text-right">
+    <div class="flex-1 text-right">
+      <div v-if="hasNext">
       <Card
         :title="props.nextTitle!"
         :description="props.nextDescription!"
         :href="props.nextHref!"
-      />
+      >
+        <template #icon >
+          <div class="flex justify-end">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-10 text-primary">
+            <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm4.28 10.28a.75.75 0 0 0 0-1.06l-3-3a.75.75 0 1 0-1.06 1.06l1.72 1.72H8.25a.75.75 0 0 0 0 1.5h5.69l-1.72 1.72a.75.75 0 1 0 1.06 1.06l3-3Z" clip-rule="evenodd" />
+            </svg>
+          </div>
+        </template>
+    </Card>
     </div>
+  </div>
   </div>
 </template>
